dd0f5306c34ccc3e37a704283ecfb7be594cb0e7
GM14032 <gm14032@ues.edu.sv>
2023-10-07 23:28:52 -0600

    change name tipo_usuario to rol

--- StadiumEventsDB.sql
+++ StadiumEventsDB.sql
@@ -1,227 +1,206 @@
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `StadiumEventsDB`
---
 DROP DATABASE IF EXISTS StadiumEventsDB;
 CREATE DATABASE StadiumEventsDB;
 USE StadiumEventsDB;
 
--- --------------------------------------------------------
 
-CREATE TABLE `asientos` (
-  `id` int(11) NOT NULL,
-  `numero` int(11) DEFAULT NULL,
-  `fila` int(11) NOT NULL,
-  `id_zona` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists auditorio
+(
+    id        int auto_increment
+        primary key,
+    nombre    varchar(200) not null,
+    capacidad int          not null,
+    ubicacion varchar(200) not null
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `zonas` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `capacidad` varchar(100) NOT NULL,
-  `ubicacion` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists evento
+(
+    id             int auto_increment
+        primary key,
+    evento         varchar(200)  not null,
+    fecha          date          not null,
+    capacidad      int           null,
+    id_evento_zona int default 1 null
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `auditorio` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `capacidad` int(11) NOT NULL,
-  `ubicacion` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists failed_jobs
+(
+    id         bigint unsigned auto_increment
+        primary key,
+    uuid       varchar(255)                        not null,
+    connection text                                not null,
+    queue      text                                not null,
+    payload    longtext                            not null,
+    exception  longtext                            not null,
+    failed_at  timestamp default CURRENT_TIMESTAMP not null,
+    constraint failed_jobs_uuid_unique
+        unique (uuid)
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `formato` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `descripcion` int(11) NOT NULL,
-  `id_auditorio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists formato
+(
+    id           int auto_increment
+        primary key,
+    nombre       varchar(200) not null,
+    descripcion  int          not null,
+    id_auditorio int          not null,
+    constraint formato_auditorio_id_auditorio_fk
+        foreign key (id_auditorio) references auditorio (id)
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `zona_formato` (
-  `id` int(11) NOT NULL,
-  `id_zona` int(11) NOT NULL,
-  `id_formato` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists migrations
+(
+    id        int unsigned auto_increment
+        primary key,
+    migration varchar(255) not null,
+    batch     int          not null
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `evento_zona` (
-  `id` int(11) NOT NULL,
-  `id_evento` int(11) NOT NULL,
-  `id_zona_formato` int(11) NOT NULL,
- `precio` decimal NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists password_reset_tokens
+(
+    email      varchar(255) not null
+        primary key,
+    token      varchar(255) not null,
+    created_at timestamp    null
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `reservas` (
-  `id` int(11) NOT NULL,
-  `dui` int(11) NOT NULL,
-  `telefono` varchar(10) NOT NULL,
-  `estado` tinyint(4) NOT NULL DEFAULT 1,
-  `leido` tinyint(4) NOT NULL,
-  `id_usuario` int(11) DEFAULT NULL,
-  `id_boleto` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists personal_access_tokens
+(
+    id             bigint unsigned auto_increment
+        primary key,
+    tokenable_type varchar(255)    not null,
+    tokenable_id   bigint unsigned not null,
+    name           varchar(255)    not null,
+    token          varchar(64)     not null,
+    abilities      text            null,
+    last_used_at   timestamp       null,
+    expires_at     timestamp       null,
+    created_at     timestamp       null,
+    updated_at     timestamp       null,
+    constraint personal_access_tokens_token_unique
+        unique (token)
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `evento` (
-  `id` int(11) NOT NULL,
-  `evento` varchar(200) NOT NULL,
-  `fecha` date NOT NULL,
-  `capacidad` int(11) DEFAULT NULL,
-  `id_evento_zona`  int(11) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create index personal_access_tokens_tokenable_type_tokenable_id_index
+    on personal_access_tokens (tokenable_type, tokenable_id);
 
-CREATE TABLE `boleto` (
-  `id` int(11) NOT NULL,
-  `fecha` datetime NOT NULL,
-  `id_evento_formato` int(11) NOT NULL,
-  `id_asiento` int(11) NOT NULL,
-  `estado` boolean NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists rol
+(
+    id     int auto_increment
+        primary key,
+    nombre varchar(100)      not null,
+    estado tinyint default 1 null
+)
+    collate = utf8mb4_unicode_ci;
 
+create table if not exists usuarios
+(
+    id                int auto_increment
+        primary key,
+    nombre            varchar(255)         not null,
+    email             varchar(255)         not null,
+    email_verified_at timestamp            null,
+    password          varchar(255)         not null,
+    dui               varchar(255)         not null,
+    telefono          varchar(255)         not null,
+    estado            tinyint(1) default 1 not null,
+    id_rol            int                  not null,
+    remember_token    varchar(100)         null,
+    created_at        timestamp            null,
+    updated_at        timestamp            null,
+    constraint usuarios_dui_unique
+        unique (dui),
+    constraint usuarios_email_unique
+        unique (email),
+    constraint usuarios_rol_id_tipo_usuario_fk
+        foreign key (id_rol) references rol (id)
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `tipo_usuario` (
-  `id` int(11) NOT NULL,
-  `tipo` varchar(100) NOT NULL,
-  `estado` tinyint(4) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists zonas
+(
+    id        int auto_increment
+        primary key,
+    nombre    varchar(100) not null,
+    capacidad varchar(100) not null,
+    ubicacion varchar(100) not null
+)
+    collate = utf8mb4_unicode_ci;
 
+create table if not exists asientos
+(
+    id      int auto_increment
+        primary key,
+    numero  int null,
+    fila    int not null,
+    id_zona int null,
+    constraint asientos_zonas_id_zona_fk
+        foreign key (id_zona) references zonas (id)
+)
+    collate = utf8mb4_unicode_ci;
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `clave` varchar(200) NOT NULL,
-  `dui` varchar(10) DEFAULT NULL unique ,
-  `telefono` varchar(15) DEFAULT NULL,
-  `email` varchar(25) DEFAULT NULL unique ,
-  `id_tipo_usuario` int(11) NOT NULL,
-  `estado` tinyint(4) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table if not exists boleto
+(
+    id                int auto_increment
+        primary key,
+    fecha             datetime   not null,
+    id_evento_formato int        not null,
+    id_asiento        int        not null,
+    estado            tinyint(1) not null,
+    constraint boleto_asiento_id_evento_fk
+        foreign key (id_asiento) references asientos (id),
+    constraint boleto_evento_id_evento_fk
+        foreign key (id_evento_formato) references evento (id)
+)
+    collate = utf8mb4_unicode_ci;
 
---
--- Índices para tablas volcadas
---
+create table if not exists reservas
+(
+    id         int auto_increment
+        primary key,
+    dui        int               not null,
+    telefono   varchar(10)       not null,
+    estado     tinyint default 1 not null,
+    leido      tinyint           not null,
+    id_usuario int               null,
+    id_boleto  int               not null,
+    constraint reservas_boletos_id_boleto_fk
+        foreign key (id_boleto) references boleto (id),
+    constraint reservas_usuarios_id_usuario_fk
+        foreign key (id_usuario) references usuarios (id)
+)
+    collate = utf8mb4_unicode_ci;
 
+create table if not exists zona_formato
+(
+    id         int auto_increment
+        primary key,
+    id_zona    int not null,
+    id_formato int not null,
+    constraint zona_formato_id_formato_fk
+        foreign key (id_formato) references formato (id),
+    constraint zona_formato_id_zona_fk
+        foreign key (id_zona) references zonas (id)
+)
+    collate = utf8mb4_unicode_ci;
 
-ALTER TABLE `asientos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `asientos_zonas_id_zona_fk` (`id_zona`);
+create table if not exists evento_zona
+(
+    id              int auto_increment
+        primary key,
+    id_evento       int     not null,
+    id_zona_formato int     not null,
+    precio          decimal not null,
+    constraint evento_formato_id_evento_fk
+        foreign key (id_evento) references evento (id),
+    constraint evento_formato_id_zona_formato_fk
+        foreign key (id_zona_formato) references zona_formato (id)
+)
+    collate = utf8mb4_unicode_ci;
 
-ALTER TABLE `zonas`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `auditorio`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `formato`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `formato_auditorio_id_auditorio_fk` (`id_auditorio`);
-
-ALTER TABLE `zona_formato`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `zona_formato_id_zona_fk` (`id_zona`),
-  ADD KEY `zona_formato_id_formato_fk` (`id_formato`);
-
-ALTER TABLE `evento_zona`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `evento_formato_id_evento_fk` (`id_evento`),
-  ADD KEY `evento_formato_id_zona_formato_fk` (`id_zona_formato`);
-
-ALTER TABLE `boleto`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `boleto_evento_id_evento_fk` (`id_evento_formato`),
-     ADD KEY `boleto_asiento_id_evento_fk` (`id_asiento`);
-
-ALTER TABLE `evento`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `reservas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `reservas_boletos_id_boleto_fk` (`id_boleto`),
- ADD KEY `reservas_usuarios_id_usuario_fk` (`id_usuario`);
-
-ALTER TABLE `tipo_usuario`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usuarios_tipo_usuario_id_tipo_usuario_fk` (`id_tipo_usuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
-ALTER TABLE `asientos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `zonas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `auditorio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `boleto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `evento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `reservas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `tipo_usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `formato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `zona_formato`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE `evento_zona`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Restricciones para tablas volcadas
---
-
-ALTER TABLE `asientos`
-  ADD CONSTRAINT `asientos_zonas_id_zona_fk` FOREIGN KEY (`id_zona`) REFERENCES `zonas` (`id`);
-
-ALTER TABLE `boleto`
-  ADD CONSTRAINT `boleto_evento_id_evento_fk` FOREIGN KEY (`id_evento_formato`) REFERENCES `evento` (`id`),
-      ADD CONSTRAINT `boleto_asiento_id_evento_fk` FOREIGN KEY (`id_asiento`) REFERENCES `asientos` (`id`);
-
-ALTER TABLE `reservas`
-ADD CONSTRAINT `reservas_boletos_id_boleto_fk` FOREIGN KEY (`id_boleto`) REFERENCES `boleto` (`id`),
-ADD CONSTRAINT `reservas_usuarios_id_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_tipo_usuario_id_tipo_usuario_fk` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id`);
-
-ALTER TABLE `formato`
-  ADD CONSTRAINT `formato_auditorio_id_auditorio_fk` FOREIGN KEY (`id_auditorio`) REFERENCES `auditorio` (`id`);
-
-ALTER TABLE `zona_formato`
-ADD CONSTRAINT `zona_formato_id_formato_fk` FOREIGN KEY (`id_formato`) REFERENCES `formato` (`id`),
-ADD CONSTRAINT `zona_formato_id_zona_fk` FOREIGN KEY (`id_zona`) REFERENCES `zonas` (`id`);
-
-ALTER TABLE `evento_zona`
-  ADD CONSTRAINT `evento_formato_id_evento_fk` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`),
-    ADD CONSTRAINT `evento_formato_id_zona_formato_fk` FOREIGN KEY (`id_zona_formato`) REFERENCES `zona_formato` (`id`);
-
-
-
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
