399662f85111a9893887ad6f5debc8d8c829f4fb
GM14032 <gm14032@ues.edu.sv>
2023-10-07 22:49:46 -0600

    v2

--- StadiumEventsDB.sql
+++ StadiumEventsDB.sql
@@ -18,39 +18,46 @@ USE StadiumEventsDB;
 
 -- --------------------------------------------------------
 
---
--- Estructura de tabla para la tabla `asientos`
---
-
 CREATE TABLE `asientos` (
   `id` int(11) NOT NULL,
-  `codigo` int(11) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
   `fila` int(11) NOT NULL,
-  `orden` int(11) NOT NULL,
-  `tipo` varchar(100) NOT NULL,
-  `estado` int(11) DEFAULT 1,
-  `id_auditorio` int(11) DEFAULT NULL
+  `id_zona` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `auditorio`
---
+CREATE TABLE `zonas` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `capacidad` varchar(100) NOT NULL,
+  `ubicacion` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `auditorio` (
   `id` int(11) NOT NULL,
   `nombre` varchar(200) NOT NULL,
   `capacidad` int(11) NOT NULL,
-  `ubicacion` varchar(200) NOT NULL,
-  `estado` int(11) DEFAULT 1
+  `ubicacion` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+CREATE TABLE `formato` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `descripcion` int(11) NOT NULL,
+  `id_auditorio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Estructura de tabla para la tabla `reservas`
---
+CREATE TABLE `zona_formato` (
+  `id` int(11) NOT NULL,
+  `id_zona` int(11) NOT NULL,
+  `id_formato` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `evento_zona` (
+  `id` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `id_zona_formato` int(11) NOT NULL,
+ `precio` decimal NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `reservas` (
   `id` int(11) NOT NULL,
@@ -62,41 +69,22 @@ CREATE TABLE `reservas` (
   `id_boleto` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `evento`
---
-
 CREATE TABLE `evento` (
   `id` int(11) NOT NULL,
   `evento` varchar(200) NOT NULL,
   `fecha` date NOT NULL,
   `capacidad` int(11) DEFAULT NULL,
-  `estado` tinyint(4) DEFAULT 1,
-  `id_auditorio` int(11) DEFAULT NULL
+  `id_evento_zona`  int(11) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `evento_boleto`
---
 
 CREATE TABLE `boleto` (
   `id` int(11) NOT NULL,
-  `tipo` varchar(100) DEFAULT NULL,
-  `precio` decimal(10,0) NOT NULL,
   `fecha` datetime NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `id_evento` int(11) NOT NULL
+  `id_evento_formato` int(11) NOT NULL,
+  `id_asiento` int(11) NOT NULL,
+  `estado` boolean NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipo_usuario`
---
 
 CREATE TABLE `tipo_usuario` (
   `id` int(11) NOT NULL,
@@ -104,16 +92,10 @@ CREATE TABLE `tipo_usuario` (
   `estado` tinyint(4) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
   `nombre` varchar(200) NOT NULL,
-  `usuario` varchar(100) NOT NULL unique ,
   `clave` varchar(200) NOT NULL,
   `dui` varchar(10) DEFAULT NULL unique ,
   `telefono` varchar(15) DEFAULT NULL,
@@ -125,50 +107,48 @@ CREATE TABLE `usuarios` (
 --
 -- Índices para tablas volcadas
 --
--- Indices de la tabla `asientos`
---
+
+
 ALTER TABLE `asientos`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `asientos_auditorio_id_auditorio_fk` (`id_auditorio`);
+  ADD KEY `asientos_zonas_id_zona_fk` (`id_zona`);
 
---
--- Indices de la tabla `auditorio`
---
+ALTER TABLE `zonas`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `auditorio`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `boleto`
---
+ALTER TABLE `formato`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `formato_auditorio_id_auditorio_fk` (`id_auditorio`);
+
+ALTER TABLE `zona_formato`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `zona_formato_id_zona_fk` (`id_zona`),
+  ADD KEY `zona_formato_id_formato_fk` (`id_formato`);
+
+ALTER TABLE `evento_zona`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `evento_formato_id_evento_fk` (`id_evento`),
+  ADD KEY `evento_formato_id_zona_formato_fk` (`id_zona_formato`);
+
 ALTER TABLE `boleto`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `boleto_evento_id:evento_fk` (`id_evento`);
+  ADD KEY `boleto_evento_id_evento_fk` (`id_evento_formato`),
+     ADD KEY `boleto_asiento_id_evento_fk` (`id_asiento`);
 
---
--- Indices de la tabla `evento`
---
 ALTER TABLE `evento`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `evento_auditorio_id_auditorio_fk` (`id_auditorio`);
-
---
--- Indices de la tabla `reservas`
---
+  ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `reservas`
   ADD PRIMARY KEY (`id`),
   ADD KEY `reservas_boletos_id_boleto_fk` (`id_boleto`),
  ADD KEY `reservas_usuarios_id_usuario_fk` (`id_usuario`);
 
---
--- Indices de la tabla `tipo_usuario`
---
 ALTER TABLE `tipo_usuario`
   ADD PRIMARY KEY (`id`);
 
---
--- Indices de la tabla `usuarios`
---
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuarios_tipo_usuario_id_tipo_usuario_fk` (`id_tipo_usuario`);
@@ -177,82 +157,69 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
---
--- AUTO_INCREMENT de la tabla `asientos`
---
 ALTER TABLE `asientos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `auditorio`
---
+ALTER TABLE `zonas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `auditorio`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `boleto`
---
 ALTER TABLE `boleto`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `evento`
---
 ALTER TABLE `evento`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `reservas`
---
 ALTER TABLE `reservas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `tipo_usuario`
---
 ALTER TABLE `tipo_usuario`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
 ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `formato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `zona_formato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `evento_zona`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
 --
 
---
--- Filtros para la tabla `asientos`
---
 ALTER TABLE `asientos`
-  ADD CONSTRAINT `asientos_auditorio_id_auditorio_fk` FOREIGN KEY (`id_auditorio`) REFERENCES `auditorio` (`id`);
+  ADD CONSTRAINT `asientos_zonas_id_zona_fk` FOREIGN KEY (`id_zona`) REFERENCES `zonas` (`id`);
 
---
--- Filtros para la tabla `boleto`
---
 ALTER TABLE `boleto`
-  ADD CONSTRAINT `boleto_evento_id_eventos_fk` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`);
-
---
--- Filtros para la tabla `evento`
---
-ALTER TABLE `evento`
-  ADD CONSTRAINT `evento_auditorio_id_auditorio_fk` FOREIGN KEY (`id_auditorio`) REFERENCES `auditorio` (`id`);
-
---
--- Filtros para la tabla `reservas`
---
+  ADD CONSTRAINT `boleto_evento_id_evento_fk` FOREIGN KEY (`id_evento_formato`) REFERENCES `evento` (`id`),
+      ADD CONSTRAINT `boleto_asiento_id_evento_fk` FOREIGN KEY (`id_asiento`) REFERENCES `asientos` (`id`);
 
 ALTER TABLE `reservas`
 ADD CONSTRAINT `reservas_boletos_id_boleto_fk` FOREIGN KEY (`id_boleto`) REFERENCES `boleto` (`id`),
 ADD CONSTRAINT `reservas_usuarios_id_usuario_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
---
--- Filtros para la tabla `usuarios`
---
+
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_tipo_usuario_id_tipo_usuario_fk` FOREIGN KEY (`id_tipo_usuario`) REFERENCES `tipo_usuario` (`id`);
+
+ALTER TABLE `formato`
+  ADD CONSTRAINT `formato_auditorio_id_auditorio_fk` FOREIGN KEY (`id_auditorio`) REFERENCES `auditorio` (`id`);
+
+ALTER TABLE `zona_formato`
+ADD CONSTRAINT `zona_formato_id_formato_fk` FOREIGN KEY (`id_formato`) REFERENCES `formato` (`id`),
+ADD CONSTRAINT `zona_formato_id_zona_fk` FOREIGN KEY (`id_zona`) REFERENCES `zonas` (`id`);
+
+ALTER TABLE `evento_zona`
+  ADD CONSTRAINT `evento_formato_id_evento_fk` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id`),
+    ADD CONSTRAINT `evento_formato_id_zona_formato_fk` FOREIGN KEY (`id_zona_formato`) REFERENCES `zona_formato` (`id`);
+
+
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
